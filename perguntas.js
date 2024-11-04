6217a83e2c0e8e62624eeed8bdb68a88cf919d01
gabrieliareski <gabriel.iareski@escola.pr.gov.br>
2024-11-04 11:49:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+'programação',
+'o que é HTML?',
+'HTML é um linguagem de marcação utilizada na construção e pagins na web'
+)
+criaCartao(
+    'Carros',
+    'Qual o melhor carro?',
+    'Pegeout 207'
+    )
+    criaCartao(
+        'Filmes',
+        'Qual a melhor recomendação?',
+        'A forja'
+        )
+        criaCartao(
+            'Esportes',
+            'Qual o melhor esporte a praticar?',
+            'Jiu-Jitsu e muay thay para defesas pessoais'
+            )
